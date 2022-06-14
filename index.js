8b8042e9a7f56d426d40129a735721ef52a844a2
Julio Junkes <junkes.julio@gmail.com>
2022-06-14 09:20:19 -0300

    updates necessários

--- index.js
+++ index.js
@@ -5,39 +5,38 @@ const path = require('path')
 const fs = require('fs')
 const app = express()
 
-// Keys generateds by webpush 
-const publicVapidKey = 'BIuz5ZRbVqHz9q1KY5_GrRJfAzgqGNyrFO9Lqq40L9GRM1UtUjCotSjofNBoXw9bN-Mb84KzmWQZkZhqf8ifdl4'
-const privateVapidKey = 'NYtqjIE0WoPLNfLCb5Z_yLKgO8_pMZpm6_wQEHJfEGU'
+// Keys generateds by webpush
+const publicVapidKey = 'BL1ymYO8-6b32YkraonEotoliOm8qF3nug_S8EN4yFnrOK9wYP3IG-Pt2fJxmLxrZATWdc6QJMvfkVy6Izra0mQ'
+const privateVapidKey = 'UMHyyAuPVgvgCnLV19ERUOZQRdQ6kLc0Ibq9j_a8kmA'
 
 const sendNotification = (subscription, message) => {
     webpush.sendNotification(subscription, message).then(
-        event => console.log(event)
+        event => null //console.log(event)
     ).catch(err => console.error(err))
 }
-
 if ( !fs.existsSync('./data/subscriptions.json') ) {
     try {
-        fs.mkdirSync('data', { mode: 0770 })
-        fs.writeFileSync('./data/subscriptions.json', '{}');
+        if (!fs.existsSync('data')) fs.mkdirSync('data', { mode: 0770 })
+        fs.writeFileSync('./data/subscriptions.json', '[]');
     } catch(err) {
         console.error(err)
     }
 } else {
     setInterval( () => {
         let subscriptionsFile = fs.readFileSync('./data/subscriptions.json')
-        let subscriptionsObject = JSON.parse(subscriptionsFile)
+        let subscriptionsArray = JSON.parse(subscriptionsFile)
         let time = new Date()
         const message = JSON.stringify({ title: 'My WebApp - Current Time', 'body': `${time.getHours()}:${time.getMinutes()}` })
-        for ( p256dh in subscriptionsObject ) {
-            let subscription  = subscriptionsObject[p256dh]            
+        for ( index in subscriptionsArray ) {
+            let subscription  = subscriptionsArray[index]
             delete subscription.time
             sendNotification( subscription, message, {TTL: 10} )
-        }    
-    }, 20000)
+        }
+    }, 60000)
 }
 
 webpush.setVapidDetails(
-  'mailto:teste@mail.com',
+  'mailto:julio@engematica.info',
   publicVapidKey,
   privateVapidKey
 );
@@ -48,21 +47,23 @@ app.use(bodyParser.json())
 
 app.post('/subscribe', (req, res) => {
     let subscriptionsFile = fs.readFileSync('./data/subscriptions.json')
-    let subscriptionsObject = JSON.parse(subscriptionsFile)
+    let subscriptionsArray = JSON.parse(subscriptionsFile)
     let now = new Date().toLocaleString()
-    let subscription = req.body
-    subscription.time = now 
-    console.log('subscribe => ', subscription.keys.p256dh)
-    subscriptionsObject[subscription.keys.p256dh] = subscription
-    subscriptionsFile = JSON.stringify(subscriptionsObject)
+    let subscription = req.body.subscription
+    subscription.clientIdentification = req.body.clientIdentification
+    subscription.time = now
+    subscriptionsArray.push(subscription)
+    subscriptionsFile = JSON.stringify(subscriptionsArray)
     fs.writeFileSync('./data/subscriptions.json', subscriptionsFile)
     res.status(201).json(subscription)
     delete subscription.time
-    const initialMessage = JSON.stringify({ 'title': 'My WebApp', 'body': 'Thank you for your subscription! :)' })
-    sendNotification(subscription, initialMessage)
+    setTimeout(() => {
+        const initialMessage = JSON.stringify({ 'title': 'My WebApp', 'body': 'Thank you for your subscription! :)' })
+        sendNotification(subscription, initialMessage)
+    }, 10000)
 })
 
-const port = 8080
+const port = 3000
 
 app.listen(port, () => console.log(`listening on port ${port}`))
 
--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-const publicVapidKey = 'BIuz5ZRbVqHz9q1KY5_GrRJfAzgqGNyrFO9Lqq40L9GRM1UtUjCotSjofNBoXw9bN-Mb84KzmWQZkZhqf8ifdl4'
+const publicVapidKey = 'BL1ymYO8-6b32YkraonEotoliOm8qF3nug_S8EN4yFnrOK9wYP3IG-Pt2fJxmLxrZATWdc6QJMvfkVy6Izra0mQ'
 
 const urlBase64ToUint8Array = base64String => {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
@@ -18,7 +18,7 @@ const urlBase64ToUint8Array = base64String => {
 const requestNotificationPermission = async () => {
 
   const permission = await window.Notification.requestPermission();
-  
+
   if(permission !== 'granted'){
       throw new Error('Permission not granted for Notification');
   }
@@ -28,7 +28,7 @@ const requestNotificationPermission = async () => {
 const sender = async () => {
 
   await requestNotificationPermission()
-    
+
   console.log('Registering serviceWorker')
   const register = await navigator.serviceWorker.register('./service-worker.js', { scope: '/' })
   console.log('Servie Worker registered')
@@ -42,23 +42,23 @@ const sender = async () => {
 		userVisibleOnly: true,
 		applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
 	})
-    console.log(subscription)
-    console.log('Push registered')
-    
-    console.log('Send register')
-    await fetch('/subscribe', {
-        'method': 'POST',
-        'body': JSON.stringify(subscription),
-        'headers': {
-            'Content-Type': 'application/json'
-        }
-    })
-    console.log('Register sent')
-	
+  // subscription.clientIdentification = 'Julio Junkes'
+  console.log(subscription)
+  console.log('Push registered')
+
+  console.log('Send register')
+  await fetch('/subscribe', {
+      'method': 'POST',
+      'body': JSON.stringify({ subscription, clientIdentification: 'Julio Junkes' }),
+      'headers': {
+          'Content-Type': 'application/json'
+      }
+  })
+  console.log('Register sent')
+
 }
 
 
 if ('serviceWorker' in navigator) {
-
     sender().catch( err => console.error(err) )
 }
